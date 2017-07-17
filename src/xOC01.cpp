183df2ebfaf4c88b78888c58847d7a7d8d0c8ef2
brendanvanbreda <brendanvanbreda@users.noreply.github.com>
2017-07-17 15:14:48 +0200

    Update xOC01.cpp

--- src/xOC01.cpp
+++ src/xOC01.cpp
@@ -4,7 +4,6 @@
 
 	The board uses I2C for communication.
 	The board communicates with the following I2C device:
-	
 	* 	PCA9536
 	
 	Data Sheets:
@@ -26,29 +25,25 @@ xOC01::xOC01()
 *********************************************************/
 bool xOC01::begin(void)
 {
-<<<<<<< HEAD
 	begin(PCA9536_ALL_OUTPUTS_OFF);
 }
 
 bool xOC01::begin(uint8_t pins)
 {
 	xCore.write8(PCA9536_I2C_ADDRESS, PCA9536_REG_OUTPUT_PORT, pins);
-=======
-	xCore.write8(PCA9536_I2C_ADDRESS, PCA9536_REG_OUTPUT_PORT, PCA9536_ALL_OUTPUTS_OFF);
->>>>>>> origin/master
 	xCore.write8(PCA9536_I2C_ADDRESS, PCA9536_REG_CONFIG, PCA9536_CONF_OUTPUT);
 }
 
 /********************************************************
  	Write to PIN
 *********************************************************/
-void xOC01::write(uint8_t pin, bool state)
+void xOC01::write(uint8_t pin, boolean state)
 {	
 	pin_state = xCore.read8(PCA9536_I2C_ADDRESS, PCA9536_REG_OUTPUT_PORT);
-	if(state == 1){
+	if(state == true){
 		pin_state |= (pin_state | (uint8_t)pin);
 		xCore.write8(PCA9536_I2C_ADDRESS, PCA9536_REG_OUTPUT_PORT, pin_state);
-	}else if(state == 0){
+	}else if(state == false){
 		pin_state &= ~(1 << pin_state|pin);
 		xCore.write8(PCA9536_I2C_ADDRESS, PCA9536_REG_OUTPUT_PORT, pin_state);
 	}
@@ -59,11 +54,7 @@ void xOC01::write(uint8_t pin, bool state)
 *********************************************************/
 uint8_t xOC01::getStatus(void)
 {
-<<<<<<< HEAD
 	pin_state = xCore.read8(PCA9536_I2C_ADDRESS, PCA9536_REG_OUTPUT_PORT); 
-=======
-	pin_state = xCore.read8(PCA9536_I2C_ADDRESS, PCA9536_REG_OUTPUT_PORT);
->>>>>>> origin/master
 	return pin_state;
 }
 
