db6a7ca2d36ad435006128d32b76a665f890bd2a
brendanvanbreda <brendanvanbreda@users.noreply.github.com>
2018-02-09 13:20:56 +0200

    Update xOC01.h

--- src/xOC01.h
+++ src/xOC01.h
@@ -16,31 +16,28 @@
 // Include File Headers
 #include "xCore.h"
 
-// PCA9536 Device I2C Address
-#define PCA9536_I2C_ADDRESS				0x41
-
 // PCA9536 Register Definitions
 #define PCA9536_REG_INPUT_PORT		0x00
 #define PCA9536_REG_OUTPUT_PORT		0x01	
 #define PCA9536_REG_POL_INVERSION	0x02
-#define PCA9536_REG_CONFIG				0x03
+#define PCA9536_REG_CONFIG			0x03
 
-#define PCA9536_CONF_OUTPUT				0x00
-#define PCA9536_CONF_INPUT				0x0F
+#define PCA9536_CONF_OUTPUT			0x00
+#define PCA9536_CONF_INPUT			0x0F
 
 #define PCA9536_ALL_OUTPUTS_OFF		0x00
 
-#define PCA9536_PIN0_OUTPUT				0x00
-#define PCA9536_PIN0_INPUT				0x01
+#define PCA9536_PIN0_OUTPUT			0x00
+#define PCA9536_PIN0_INPUT			0x01
 
-#define PCA9536_PIN1_OUTPUT				0x00
-#define PCA9536_PIN1_INPUT				0x02
+#define PCA9536_PIN1_OUTPUT			0x00
+#define PCA9536_PIN1_INPUT			0x02
 
-#define PCA9536_PIN2_OUTPUT				0x00
-#define PCA9536_PIN2_INPUT				0x04
+#define PCA9536_PIN2_OUTPUT			0x00
+#define PCA9536_PIN2_INPUT			0x04
 
-#define PCA9536_PIN3_OUTPUT				0x00
-#define PCA9536_PIN3_INTPUT				0x08
+#define PCA9536_PIN3_OUTPUT			0x00
+#define PCA9536_PIN3_INTPUT			0x08
 
 #define OUT0 						0x01
 #define OUT1 						0x02
@@ -98,10 +95,11 @@ class xOC01: public xCoreClass
 		uint8_t getStatus(void);
 		
 	private:
+	
+		// PCA9536 Device I2C Address
+		uint8_t PCA9536_I2C_ADDRESS;
 
 		uint8_t pin_state;
 };
-
-extern xOC01 OC01;
 
 #endif
